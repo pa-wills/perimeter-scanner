24d5e94199eecc78c536838060396e4bfd0fd851
Pete Wills <34256848+pa-wills@users.noreply.github.com>
2022-11-15 16:54:35 +1100

    Subdomain script enhanced
    Now allows for multiple domains, properly parameterised.

--- enumerateSubdomains.bash
+++ enumerateSubdomains.bash
@@ -9,6 +9,8 @@
 
 
 # Note relies on ENV VARs set by EC2 at launchtime. Specifically: PSCAN_RECONNG_WORKSPACE, PSCAN_S3_BUCKET, etc.
+domains = $(echo $PSCAN_DOMAINS_TO_ENUMERATE | tr "," "\n")
+
 
 cd /home/ec2-user/recon-ng/
 /home/ec2-user/recon-ng/recon-cli -C "workspaces remove $PSCAN_RECONNG_WORKSPACE" --no-version
@@ -19,55 +21,56 @@ sleep 10
 
 /home/ec2-user/recon-ng/recon-cli -C "marketplace install brute_hosts" --no-version
 /home/ec2-user/recon-ng/recon-cli -C "modules load brute_hosts" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/brute_hosts -c "options set SOURCE nbnco.com.au" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/brute_hosts -c "run" --no-version
-sleep 10
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/brute_hosts -c "options set SOURCE nbnco.net.au" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/brute_hosts -c "run" --no-version
-sleep 10
+for domain in $domains
+do
+	/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/brute_hosts -c "options set SOURCE $domain" --no-version
+	/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/brute_hosts -c "run" --no-version
+	sleep 10
+done
 
 /home/ec2-user/recon-ng/recon-cli -C "marketplace install hackertarget" --no-version
 /home/ec2-user/recon-ng/recon-cli -C "modules load hackertarget" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/hackertarget -c "options set SOURCE nbnco.com.au" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/hackertarget -c "run" --no-version
-sleep 10
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/hackertarget -c "options set SOURCE nbnco.net.au" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/hackertarget -c "run" --no-version
-sleep 10
+for domain in $domains
+do
+	/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/hackertarget -c "options set SOURCE $domain" --no-version
+	/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/hackertarget -c "run" --no-version
+	sleep 10
+done
 
 /home/ec2-user/recon-ng/recon-cli -C "marketplace install certificate_transparency" --no-version
 /home/ec2-user/recon-ng/recon-cli -C "modules load certificate_transparency" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/certificate_transparency -c "options set SOURCE nbnco.com.au" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/certificate_transparency -c "run" --no-version
-sleep 10
-
-# TODO: This is _not_ working. Why?
-/home/ec2-user/recon-ng/recon-cli -m recon/domains-hosts/certificate_transparency -c "options set SOURCE nbnco.net.au" --no-version
-/home/ec2-user/recon-ng/recon-cli -m recon/domains-hosts/certificate_transparency -c "run" --no-version
-sleep 10
+for domain in $domains
+do
+	/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/certificate_transparency -c "options set SOURCE $domain" --no-version
+	/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/certificate_transparency -c "run" --no-version
+	sleep 10
+done
 
 /home/ec2-user/recon-ng/recon-cli -C "marketplace install bing_domain_web" --no-version
 /home/ec2-user/recon-ng/recon-cli -C "modules load bing_domain_web" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/bing_domain_web -c "options set SOURCE nbnco.com.au" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/bing_domain_web -c "run" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/bing_domain_web -c "options set SOURCE nbnco.net.au" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/bing_domain_web -c "run" --no-version
+for domain in $domains
+do
+	/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/bing_domain_web -c "options set SOURCE $domain" --no-version
+	/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/bing_domain_web -c "run" --no-version
+	sleep 10
+done
 
 /home/ec2-user/recon-ng/recon-cli -C "marketplace install google_site_web" --no-version
 /home/ec2-user/recon-ng/recon-cli -C "modules load google_site_web" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/google_site_web -c "options set SOURCE nbnco.com.au" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/google_site_web -c "run" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/google_site_web -c "options set SOURCE nbnco.net.au" --no-version
-/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/google_site_web -c "run" --no-version
+for domain in $domains
+do
+	/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/google_site_web -c "options set SOURCE $domain" --no-version
+	/home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m recon/domains-hosts/google_site_web -c "run" --no-version
+	sleep 10
+done
 
 /home/ec2-user/recon-ng/recon-cli -C "marketplace install csv" --no-version
 /home/ec2-user/recon-ng/recon-cli -C "modules load reporting/csv" --no-version
 /home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m reporting/csv -c "options set HEADERS True" --no-version
 /home/ec2-user/recon-ng/recon-cli -w $PSCAN_RECONNG_WORKSPACE -m reporting/csv -c "run" --no-version
+
 aws s3 cp /home/ec2-user/.recon-ng/workspaces/$PSCAN_RECONNG_WORKSPACE/results.csv s3://$PSCAN_S3_BUCKET
 rm /home/ec2-user/.recon-ng/workspaces/$PSCAN_RECONNG_WORKSPACE/results.csv
-
-# TODO: take the name of the s3 bucket from aws itself, not set it statically. This is a crude hack.
 
 
 
